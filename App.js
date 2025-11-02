d0e556b34f0458e7dbedbf878f968a86459b3eb5
cinna-vk <naomi.ortiz.juarez@gmail.com>
2025-11-02 13:32:54 -0600

    use state done

--- App.js
+++ App.js
@@ -1,22 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { estiloTextos } from './misEstilos';
+import { Text, View, Button, TextInput, FlatList } from 'react-native';
+import { estilos } from './misEstilos';
+import { useState } from 'react';
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+  const [nombre, setNombre] = useState('');
+  const [listaNombres, setListaNombres] = useState([]);
+  const longitud = nombre.length;
+  const emoji =
+    longitud === 0 ? '😞' :
+    longitud < 5 ? '🙂' :
+    longitud < 10 ? '😃' :
+    '😆';
+
+  const guardarnombre = () => {
+    if (nombre.trim()) {
+      setListaNombres([...listaNombres, nombre]);
+      setNombre('');
+    }
+  };
+  const borrarNombre = (index) => {
+    setListaNombres(listaNombres.filter((_, i) => i !== index));
+  }
+  const limpiarLista = () => {
+    setListaNombres([]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text style={estiloTextos.texto}>Hola, mundo</Text>
+    <View style={estilos.container}>
+      <Text>Clicks: {contador}</Text>
+      <Button title="Click" onPress={() => setContador(c => c + 1)} />
       <StatusBar style="auto" />
+
+      <TextInput style={estilos.input}
+        placeholder="Escribe tu nombre"
+        value={nombre}
+        onChangeText={setNombre}
+      />
+      <Text>Tu nombre tiene: {longitud} caracteres {emoji}</Text>
+      <Button title="Guardar nombre" onPress={guardarnombre}/>
+
+      <View style={estilos.listaContainer}>
+        <Text>Nombres guardados:</Text>
+        <FlatList
+          data={listaNombres}
+          renderItem={({item, index}) => (
+            <View style={estilos.listaItem}>
+              <Text>{index + 1}. {item}</Text>
+              <Button title="Borrar" onPress={() => borrarNombre(index)} />
+            </View>
+          )}
+          
+          keyExtractor={(_, index) => index.toString()}
+        />
+      </View>
+      <Button title="Limpiar lista" onPress={() => limpiarLista()} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
--- misEstilos.js
+++ misEstilos.js
@@ -1,7 +1,25 @@
 import { StyleSheet } from "react-native/";
 
-export const estiloTextos = StyleSheet.create({
+export const estilos = StyleSheet.create({
     texto: {
         color: '#BD93BD'
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    listaContainer: {
+        marginTop: 20,
+        width: '80%',
+        maxHeight: 200,
+    },
+    listaItem: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 5,
+        fontSize: 14,
     }
-})
+});
